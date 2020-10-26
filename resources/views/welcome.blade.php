a57fba7faeef2c698da33578e549aeb26fc7ae27
faiyazkhanwif <faiyazkhanwif@gmail.com>
2020-10-27 05:05:21 +0800

    CSS seperated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,22 +3,22 @@
 @section('content')
 <div class="flex-center position-ref full-height">
     @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
+    <div class="top-right links">
+        @auth
+        <a href="{{ url('/home') }}">Home</a>
+        @else
+        <a href="{{ route('login') }}">Login</a>
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                @endif
-            @endauth
-        </div>
+        @if (Route::has('register'))
+        <a href="{{ route('register') }}">Register</a>
+        @endif
+        @endauth
+    </div>
     @endif
 
     <div class="content">
+        <img src="/img/pizza-house.png" alt="pizza-house logo">
         <div class="title m-b-md">
-            Pizza House<br />
             The North's Best Pizzas
         </div>
     </div>
